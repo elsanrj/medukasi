67b4a9c2c57c4ba21b7e6db97b5c6784f6444dfd
Naeya Adeani <naeyaap@upi.edu>
2025-04-22 21:27:07 +0700

    cobalagi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Tes Laravel Medukasi coba</title>
+        <title>Tes Laravel Medukasi coba lagi lagi</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
