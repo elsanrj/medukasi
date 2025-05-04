804c0f34688b7af03b112057ea88258adbcffa35
Shizuka Maulia Putri <mauliaputri23@gmail.com>
2025-05-04 20:28:57 +0700

    Update DashboardPimpinan.jsx

--- frontend/src/pages/DashboardPimpinan.jsx
+++ frontend/src/pages/DashboardPimpinan.jsx
@@ -42,7 +42,7 @@ const DashboardPimpinan = () => {
 
 
   return (
-    <div className="flex flex-col min-h-screen bg-[#fdf5ff]">
+    <div className="flex flex-col min-h-screen bg-purple-50">
       <HeaderAdmin toggleSidebar={toggleSidebar} />
       <div className="flex flex-1 overflow-hidden">
         {/* Sidebar */}
